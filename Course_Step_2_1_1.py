9beeaa1a46256c54762d4fd1118b1e23ff5e6b08
vnv <vavilovnv@gmail.com>
2020-12-18 16:19:02 +0300

    Create Course_Step_2_1_1.py

---
+++ Course_Step_2_1_1.py
@@ -0,0 +1,10 @@
+# Условие задачи: https://stepik.org/lesson/24463/step/6?auth=login&thread=solutions&unit=6771
+
+try:
+    foo()
+except ZeroDivisionError :
+    print("ZeroDivisionError")
+except ArithmeticError:
+    print("ArithmeticError")
+except AssertionError:
+    print("AssertionError")
