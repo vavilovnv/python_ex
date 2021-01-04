702926a4069beb02147acbf8b76c88c4bb61f886
Nikolay <53932721+vnv@users.noreply.github.com>
2021-01-04 21:19:26 +0300

    another task on regular expressions has been added

---
+++ Course - Python fundamentals and applications/Course_Step_3_3_2.py
@@ -0,0 +1,17 @@
+# Текст задания: https://stepik.org/lesson/24471/step/7?unit=6780
+
+import requests
+import re
+
+pattern = r'(<a.*href[ ]?=[ ]? ?.)(http:\/\/|https:\/\/|ftp:\/\/)?([\w._-]*)'
+text = requests.get(input().strip()'').text # 'https://pastebin.com/raw/7543p0ns'
+
+urls, urls_out = re.findall(pattern, text), set()
+for url in urls:
+    urls_out.add(url[2])
+
+for url in sorted(urls_out):
+    if url != '..':
+        print(url)
+
+
