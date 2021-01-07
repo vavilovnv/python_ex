74192fe38e61558fa6590f5f74c07165be3045c1
Nikolay <53932721+vnv@users.noreply.github.com>
2021-01-07 17:10:33 +0300

    bug fixing

--- Course - Python fundamentals and applications/Course_Step_3_4_2.py
+++ Course - Python fundamentals and applications/Course_Step_3_4_2.py
@@ -2,27 +2,28 @@
 
 import json
 
-def count_children(ans, cls):
+def is_ancestor(ans, cls):
     if ans in classes[cls]:
-        return 1
+        return True
     else:
-        check = 0
+        check = False
         for i in classes[cls]:
             if i != cls:
-                check = count_children(ans, i)
-                if check == 1:
-                    return 1
-        return 0
+                check = is_ancestor(ans, i)
+                if check:
+                    return True
+        return False
 
-str_classes = '[{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]'
-data_classes = json.loads(str_classes) # input()
+data_classes = json.loads(input())
 
 classes = {}
 for eq in data_classes:
     classes[eq['name']] = eq['parents']
-    classes[eq['name']] += eq['name']
 
-for cls in classes:
-    print(cls, ':', count_children(, cls))
+for c in sorted(classes.keys()):
+    count = 0
+    for p in classes:
+        count += 1 if is_ancestor(c, p) else 0
+    print(c, ':', count + 1)
 
 
