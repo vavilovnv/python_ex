bf2a3adbaba52fd992393183a392dada9d8cea00
Nikolay <53932721+vnv@users.noreply.github.com>
2021-01-12 17:39:00 +0300

    Added task descriptions and corrected pep8 warnings

--- Course - Python basic and applications/Step_2_1_1.py
+++ Course - Python basic and applications/Step_2_1_1.py
@@ -1,8 +1,19 @@
 # Условие задачи: https://stepik.org/lesson/24463/step/6?auth=login&thread=solutions&unit=6771
 
+# Вашей программе будет доступна функция foo, которая может бросать исключения.
+# Вам необходимо написать код, который запускает эту функцию, затем ловит исключения ArithmeticError, AssertionError,
+# ZeroDivisionError и выводит имя пойманного исключения.
+# Пример решения, которое вы должны отправить на проверку.
+# try:
+#     foo()
+# except Exception:
+#     print("Exception")
+# except BaseException:
+#     print("BaseException")
+
 try:
     foo()
-except ZeroDivisionError :
+except ZeroDivisionError:
     print("ZeroDivisionError")
 except ArithmeticError:
     print("ArithmeticError")
--- Course - Python basic and applications/Step_2_1_3.py
+++ Course - Python basic and applications/Step_2_1_3.py
@@ -1,7 +1,17 @@
 # Условие задачи: https://stepik.org/lesson/24463/step/9?auth=login&unit=6771
 
+# Реализуйте класс PositiveList, отнаследовав его от класса list, для хранения положительных целых чисел.
+# Также реализуйте новое исключение NonPositiveError.
+# В классе PositiveList переопределите метод append(self, x) таким образом, чтобы при попытке добавить неположительное
+# целое число бросалось исключение NonPositiveError и число не добавлялось, а при попытке добавить положительное целое
+# число, число добавлялось бы как в стандартный list.
+# В данной задаче гарантируется, что в качестве аргумента x метода append всегда будет передаваться целое число.
+# Примечание:
+# Положительными считаются числа, строго большие нуля.
+
 class NonPositiveError(Exception):
     pass
+
 
 class PositiveList(list):
     def append(self, n):
@@ -9,3 +19,4 @@ class PositiveList(list):
             super().append(n)
         else:
             raise NonPositiveError
+
