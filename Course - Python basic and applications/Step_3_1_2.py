e75c751b061fe5d396e799d72a5b1400d08abda4
Nikolay <53932721+vnv@users.noreply.github.com>
2021-01-13 18:38:13 +0300

    Task descriptions added

--- Course - Python basic and applications/Step_3_1_2.py
+++ Course - Python basic and applications/Step_3_1_2.py
@@ -1,4 +1,14 @@
 # Текст задания: https://stepik.org/lesson/24469/step/7?unit=6775
 
+# Вашей программе на вход подаются две строки s и t, состоящие из строчных латинских букв.
+# Выведите одно число – количество вхождений строки t в строку s.
+# Пример:
+# s = "abababa"
+# t = "aba"
+# Вхождения строки t в строку s:
+# abababa
+# abababa
+# abababa
+
 s, t = (input() for i in range(2))
 print(sum(1 for i in range(len(s) - len(t)+1) if s.find(t, i, i + len(t)) >= 0))
