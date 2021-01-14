110f37657ef37abf32a3e487edc5d902091d0f02
Nikolay <53932721+vnv@users.noreply.github.com>
2021-01-14 23:17:27 +0300

    Task descriptions added

--- Course - Python basic and applications/Step_3_2_1.py
+++ Course - Python basic and applications/Step_3_2_1.py
@@ -1,5 +1,8 @@
 # Текст задания: https://stepik.org/lesson/24470/step/7?unit=6776
 
+# Вам дана последовательность строк.
+# Выведите строки, содержащие "cat" в качестве подстроки хотя бы два раза.
+
 import sys
 import re
 
@@ -7,4 +10,5 @@ for line in sys.stdin:
     line = line.rstrip()
     if len(re.findall(r'(cat)', line)) >= 2:
         print(line)
+
 
