06e1da9500577ecb90a5aecdaa962b8f27a31138
Nikolay <53932721+vnv@users.noreply.github.com>
2021-01-15 21:09:36 +0300

    The problem to calculate the growth rate of functions

---
+++ Course - Algorithms theory and practice. Methods/Step_2_4_3.py
@@ -0,0 +1,19 @@
+# Текст задания: https://stepik.org/lesson/13230/step/9?unit=3416
+
+import math
+
+n = 5000
+
+print(pow(4, n))
+print(pow(n, 3))
+print(n * (pow(math.log(n, 2), 3)))
+print(n * math.log(n, 2))
+print(math.sqrt(n))
+print(pow(n, 0.3))
+print(math.log(n, 5))
+
+
+
+
+
+
