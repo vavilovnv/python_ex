fa6a6955977198ab66e5f19bdfd16f8cf4ec63b8
Nikolay <53932721+vnv@users.noreply.github.com>
2021-01-19 22:24:52 +0300

    A couple of tasks were done

---
+++ Course - Adaptive Python Trainer/Step_9_1.py
@@ -0,0 +1,42 @@
+# Текст задания: https://stepik.org/lesson/22985/step/1?adaptive=true&unit=5369
+
+# A durak deck contains 36 cards. Each card has a suit of either clubs, diamonds, hearts, or spades
+# (denoted C, D, H, S). Each card also has a value of either 6 through 10, jack, queen, king, or ace
+# (denoted 6, 7, 8, 9, 10, J, Q, K, A). For scoring purposes card values are ordered as above, with 6 having the lowest
+# and ace the highest value.
+# Напишите программу, которая определяет, бьёт ли одна карта другую.
+# Если встречаются две карты одной масти, то побеждает та, у которой выше значение;
+# Если карты разных мастей, то карта, имеющая козырную масть, побеждает;
+# Если карты разных мастей и нет козырных, то никто не побеждает.
+# Формат ввода:
+# На первой строке через пробел указываются две карты в формате <значение><масть>, а на следующей строке указывается козырная масть.
+# Формат вывода:
+# Программа должна вывести слово
+# First, если первая карта бьёт вторую,
+# Second, если вторая карта бьёт первую,
+# Error, если ни одна из карт не может побить другую.
+
+cards, w = input().split(), input()
+value = {"6":6, "7":7, "8":8, "9":9, "10":10, "J":11, "Q":12, "K":13, "A":14}
+
+m1, m2 = cards[0][-1], cards[1][-1]
+v1 = cards[0][0:len(cards[0])-1]
+v2 = cards[1][0:len(cards[1])-1]
+if m1 == m2:
+    if value[v1] > value[v2]:
+        print("First")
+    elif value[v1] < value[v2]:
+        print("Second")
+    else:
+        print("Error")
+elif m1 != w and m2 != w:
+    print("Error")
+else
+    m1 == w:
+    print("First")
+elif m2 == w:
+    print("Second")
+else:
+
+
+
--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 :hankey: ~Осторожно, много говнокода созданного в процессе обучения~
 
 Решение задач из курсов stepik.org по программированию на Python:
-- [x] 1 [Programming with Python](https://stepik.org/course/67/syllabus)
-- [x] 2 [Python fundamentals and applications](https://stepik.org/course/512/syllabus)
-- [ ] 3 [Algorithms: theory and practice. Methods](https://stepik.org/course/217/syllabus)
-- [ ] 4 [Adaptive Python Trainer](https://stepik.org/course/431/info)
+- [x] [Programming with Python](https://stepik.org/course/67/syllabus)
+- [x] [Python fundamentals and applications](https://stepik.org/course/512/syllabus)
+- [ ] [Algorithms: theory and practice. Methods](https://stepik.org/course/217/syllabus)
+- [ ] [Adaptive Python Trainer](https://stepik.org/course/431/info)
+
+![Python](https://img.shields.io/badge/-Python-blue)
