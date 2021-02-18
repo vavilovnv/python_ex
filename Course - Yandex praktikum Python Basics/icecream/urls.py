9985a04d7cc9fb590a07cc4396e12c57c855207b
Nikolay <53932721+vnv@users.noreply.github.com>
2021-02-19 00:18:48 +0300

    Project files updated

--- Course - Yandex praktikum Python Basics/icecream/urls.py
+++ Course - Yandex praktikum Python Basics/icecream/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.icecream_list),
-    # Добавьте сюда path() с переменной pk типа int и вызовом icecream_detail()
+    path('<int:pk>/', views.icecream_detail),
 ]
--- Course - Yandex praktikum Python Basics/icecream/views.py
+++ Course - Yandex praktikum Python Basics/icecream/views.py
@@ -5,20 +5,16 @@ from .models import icecream_db
 def icecream_list(request):
     icecreams = ''
     for i in range(len(icecream_db)):
-        icecreams += f"{icecream_db[i]['name']}<br>"
+        icecreams += f"<a href='{i}/'>{icecream_db[i]['name']}</a><br>"
     context = {
         'icecreams': icecreams,
     }
     return render(request, 'icecream/icecream-list.html', context)
 
 
-# Вторым аргументом эта функция должна принять переменную pk из path()
-def icecream_detail(request):
-    # Получите из icecream_db отдельно название
-    # и отдельно описание запрошенного мороженого.
-    # Сохраните их, соответственно, в переменные name и description
-    name = ...
-    description = ...
+def icecream_detail(request, pk):
+    name = icecream_db[pk]['name']
+    description = icecream_db[pk]['description']
     context = {
         'name': name,
         'description': description,
