55604b6f5713be6ce7d8bcf08595660cda5c8a0d
Nikolay <53932721+vnv@users.noreply.github.com>
2021-06-17 00:24:39 +0300

    Update tkinter.py

--- Test tasks (unsorted)/tkinter.py
+++ Test tasks (unsorted)/tkinter.py
@@ -2,8 +2,15 @@
 
 from tkinter import *
 
+def clicked():
+    lbl.configure(text='Great!')
+
+
 window = Tk()
 window.title('Window title')
+window.geometry('400x200')
 lbl = Label(window, text='Hello world!', font=('Calibry', 25))
 lbl.grid(column=0, row=0)
+btn = Button(window, text='Press me', command = clicked, bg='gray', fg='black')
+btn.grid(column=1, row=0)
 window.mainloop()  # без этого бесконечного цикла окно не отобразится
