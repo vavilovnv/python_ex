199256d5528311818d6c3e049df3a85b707cfca4
Nikolay <53932721+vavilovnv@users.noreply.github.com>
2022-02-02 22:37:41 +0300

    A couple of tasks were solved

---
+++ Course - Python Generation, A Course for Beginners/Step_15_5_4.py
@@ -0,0 +1,33 @@
+# Текст задания: https://stepik.org/lesson/352860/step/4?unit=336821
+
+# требуется написать программу, способную шифровать и дешифровать текст в соответствии с алгоритмом Цезаря. Она должна
+# запрашивать у пользователя следующие данные:
+#
+# направление: шифрование или дешифрование;
+# язык алфавита: русский или английский;
+# шаг сдвига (со сдвигом вправо).
+
+def make_shift(c, dct):
+    return dct[dct.find(c) + shift - len(dct)]
+
+
+def caesar_code():
+    new_s = ''
+    if alphabet == 'ru':
+        alphabet_upper, alphabet_lower  = upper_ru, lower_ru
+    else:
+        alphabet_upper, alphabet_lower = upper_en, lower_en
+    for c in s:
+        if c in alphabet_upper:
+            new_s += make_shift(c, alphabet_upper)
+        elif c in alphabet_lower:
+            new_s += make_shift(c, alphabet_lower)
+        else:
+            new_s += c
+    return new_s
+
+
+lower_ru, lower_en = 'абвгдежзийклмнопрстуфхцчшщъыьэюя', 'abcdefghijklmnopqrstuvwxyz'
+upper_ru, upper_en = lower_ru.upper(), lower_en.upper()
+s, alphabet, shift = input('Введите строку: '), input('Укажите язык (ru/en): '), int(input('Укажите сдвиг: '))
+print(caesar_code())
