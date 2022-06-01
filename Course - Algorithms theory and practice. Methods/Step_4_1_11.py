8bddd56b484fef06bdc647019f8fe6592426cf22
Nikolay <53932721+vavilovnv@users.noreply.github.com>
2022-06-01 23:57:15 +0300

    Refactoring

--- Course - Algorithms theory and practice. Methods/Step_4_1_11.py
+++ Course - Algorithms theory and practice. Methods/Step_4_1_11.py
@@ -1,18 +1,24 @@
 # Условие задачи https://stepik.org/lesson/13238/step/11?unit=3424
 
-# По данному числу 1≤n≤10^9 найдите максимальное число k, для которого n можно представить как сумму k различных
-# натуральных слагаемых. Выведите в первой строке число k, во второй — k слагаемых.
+# По данному числу 1≤n≤10^9 найдите максимальное число k, для которого n можно
+#  представить как сумму k различных натуральных слагаемых. Выведите в первой
+# строке число k, во второй — k слагаемых.
 
-n = int(input())
-summations, i = [], 1
-while n - i > i:
-    summations.append(i)
-    n -= i
-    i += 1
-summations.append(n)
-print(len(summations))
-print(*summations)
+def different_summations(num: int) -> list:
+    '''Функция формирует максимальный по количеству элементов список из
+    k-слагаемых для переданного числа num. Алгоритм основан на принципе
+    жадного алгоритма. За достоверный шаг берется выбор наименьшего
+    слагаемоего.'''
+    summations, i = [], 1
+    while num - i > i:
+        summations.append(i)
+        num -= i
+        i += 1
+    summations.append(num)
+    return summations
 
 
-
-
+if __name__ == '__main__':
+    summations = different_summations(int(input()))
+    print(len(summations))
+    print(*summations)
