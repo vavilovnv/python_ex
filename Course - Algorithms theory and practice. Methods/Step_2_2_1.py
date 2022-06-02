c5270949b077daee9ddef1becca44e99d9fa6608
Nikolay <53932721+vavilovnv@users.noreply.github.com>
2022-06-02 16:21:21 +0300

    Refactoring

--- Course - Algorithms theory and practice. Methods/Step_2_2_1.py
+++ Course - Algorithms theory and practice. Methods/Step_2_2_1.py
@@ -1,19 +1,15 @@
 # Текст задания: https://stepik.org/lesson/13228/step/6?unit=3414
 
-# Дано целое число 1 ≤ n ≤ 40, необходимо вычислить n-е число Фибоначчи (напомним, что F_0=0, F_1=1,
-# F_n = F_n-1 + F_n-2 при n ≥ 2.
+# Дано целое число 1 ≤ n ≤ 40, необходимо вычислить n-е число Фибоначчи
+# (напомним, что F_0 = 0, F_1 = 1, F_n = F_n-1 + F_n-2 при n ≥ 2).
 
 def fib(n):
     a, b = 0, 1
-
-    for i in range(1, n):
+    for _ in range(1, n):
         a, b = b, a + b
     return b
 
-def main():
-    n = int(input())
-    print(fib(n))
-
 
 if __name__ == "__main__":
-    main()
+    n = int(input())
+    print(fib(n))
--- Course - Algorithms theory and practice. Methods/Step_2_4_3.py
+++ Course - Algorithms theory and practice. Methods/Step_2_4_3.py
@@ -1,19 +1,30 @@
 # Текст задания: https://stepik.org/lesson/13230/step/9?unit=3416
 
+# Упорядочите данные семь функций по возрастанию скорости роста (сверху —
+# медленнее всего растущая функция, снизу — быстрее всего растущая).
+
+import time
 import math
 
-n = 5000
 
-print(pow(4, n))
-print(pow(n, 3))
-print(n * (pow(math.log(n, 2), 3)))
-print(n * math.log(n, 2))
-print(math.sqrt(n))
-print(pow(n, 0.3))
-print(math.log(n, 5))
-
-
-
-
+def timed(f, *args, count_iter=100):
+    '''Функция рассчитывает минимальное время исполнения переданной
+    функции за определенное количество итераций'''
+    acc = float('inf')
+    for i in range(count_iter):
+        t0 = time.perf_counter()
+        f(*args)
+        t1 = time.perf_counter()
+        acc = min(acc, t1 - t0)
+    return acc
 
 
+if __name__ == '__main__':
+    n = 5000
+    print(timed(math.log, n, 5))
+    print(timed(pow, n, 0.3))
+    print(timed(math.sqrt, n))
+    print(n * timed(math.log, n, 2))
+    print(n * timed(pow, math.log(n, 2), 3))
+    print(timed(pow, n, 3))
+    print(timed(pow, 4, n))
