b98f5d836b0d75877ea9ec0cd1b54742c4ae5364
Николай <53932721+vavilovnv@users.noreply.github.com>
2023-03-29 15:19:24 +0300

    Tasks from the first yandex interview

---
+++ Test tasks (unsorted)/Solutions/my-interviews/yandex-1.py
@@ -0,0 +1,59 @@
+"""
+Дан список целых чисел, повторяющихся элементов в списке нет.
+Нужно преобразовать это множество в строку, 
+сворачивая соседние по числовому ряду числа в диапазоны.
+
+Примеры:
+- [1, 4, 5, 2, 3, 9, 8, 11, 0] => "0-5,8-9,11"
+- [1, 4, 3, 2] => "1-4"
+- [1, 4] => "1,4"
+"""
+
+def group_out(g_start, g_end):
+    if g_start == g_end:
+        return str(g_end)
+    return f'{g_start}-{g_end}'
+
+
+def compress(l):
+    if not l:
+        return ''
+    l = sorted(l)
+    res, g_start, g_end = [], l[0], l[0]
+    for n in l[1:]:
+        if g_end == n-1:
+            g_end = n
+        else:
+            res.append(group_out(g_start, g_end))
+            g_start = n
+            g_end = n
+    res.append(group_out(g_start, g_end))
+    return ','.join(res)
+
+
+"""
+Написать стек с константным поиском максимального элемента.
+
+push, pop, max должны работать за O(1).
+"""
+class MaxStack(object):
+    def __init__(self):
+        self.stack = []
+ 
+    def push(self, elem):
+        if not self.stack:
+            self.stack.append((elem, elem))
+        else:
+            m = max(self.stack[-1][1], elem)    
+            self.stack.append((elem, m))        
+
+    def pop(self) -> int:
+        if self.stack:
+            m = self.stack.pop()
+            return m[0]
+        raise Exception('Стек пустой')
+
+    def max(self) -> int:
+        if self.stack:
+            return self.stack[-1][1]
+        raise Exception('Стек пустой')
