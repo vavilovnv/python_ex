5daf12dc0721e90d3bccc8a3817072c17d3760ad
Николай <53932721+vavilovnv@users.noreply.github.com>
2023-04-02 16:46:07 +0300

    Solution optimized

--- Test tasks (unsorted)/Solutions/leetcode/733-Flood-fill.py
+++ Test tasks (unsorted)/Solutions/leetcode/733-Flood-fill.py
@@ -21,21 +21,23 @@ Return the modified image after performing the flood fill.
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         def helper(row, column):
-            if image[row][column] == color:
+            if (not (0 <= row < m and 0 <= column < n)
+                or image[row][column] != prev_color):
                 return
             image[row][column] = color
-            if row-1 >= 0 and image[row-1][column] == prev_color:
-                helper(row-1, column)
-            if row+1 < m and image[row+1][column] == prev_color:
-                helper(row+1, column)
-            if column-1 >= 0 and image[row][column-1] == prev_color:
-                helper(row, column-1)
-            if column + 1 < n and image[row][column+1] == prev_color:
-                helper(row, column+1)
+            helper(row - 1, column)
+            helper(row + 1, column)
+            helper(row, column - 1)
+            helper(row, column + 1)
+        
         prev_color = image[sr][sc]
-        if prev_color == color:
-            return image
-        m, n = len(image), len(image[0])
-        helper(sr, sc)
+        if prev_color != color:
+            m, n = len(image), len(image[0])
+            helper(sr, sc)
         return image
-            
+
+"""
+hint
+use bfs while 0 <= row < m and 0 <= column < n and
+image[row][column] == prev_color
+"""
