cac59769c7f159bebfa1fc2dedac756fb76480b3
Николай <53932721+vavilovnv@users.noreply.github.com>
2023-04-08 10:13:51 +0300

    O(n) solution added

--- Test tasks (unsorted)/Solutions/leetcode/283-Move-zeroes.py
+++ Test tasks (unsorted)/Solutions/leetcode/283-Move-zeroes.py
@@ -9,6 +9,7 @@ the relative order of the non-zero elements.
 Note that you must do this in-place without making a copy of the array.
 """
 
+# O(n^2)
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         left, right = 0, len(nums)
@@ -20,7 +21,20 @@ class Solution:
             else:
                 left += 1
 
-"""
-hint
-if the element is 0 pop it and append to end
-"""
+# O(n)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        num_index = 0
+        for i, v in enumerate(nums):
+            if v != 0:
+                num_index += 1
+            else:
+                break
+        for i in range(num_index, len(nums)):
+            if nums[i] != 0:
+                nums[num_index], nums[i] = nums[i], nums[num_index]
+                num_index += 1
+        return nums
