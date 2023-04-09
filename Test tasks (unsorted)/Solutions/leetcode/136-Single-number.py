13ed39d7cde48c83762a44d606b8a5d4ab8e811c
Николай <53932721+vavilovnv@users.noreply.github.com>
2023-04-09 19:12:24 +0300

    New solution added

--- Test tasks (unsorted)/Solutions/leetcode/136-Single-number.py
+++ Test tasks (unsorted)/Solutions/leetcode/136-Single-number.py
@@ -10,6 +10,7 @@ You must implement a solution with a linear runtime complexity and use only
 constant extra space.
 """
 
+# Complexity O(n), memory O(n)
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         seek = {}
@@ -19,3 +20,12 @@ class Solution:
             else:
                 seek[num] += 1
         return [k for k, v in seek.items() if v == 1][0]
+    
+# Complexity O(n), memory O(1)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        # XORing a number with itself gives 0
+        for num in nums:
+            res ^= num
+        print(res)
