18ee4485b99c2d1eb5297e7d039233b1f4e0a578
Николай <53932721+vavilovnv@users.noreply.github.com>
2023-04-13 19:51:12 +0300

    Another solution added

--- Test tasks (unsorted)/Solutions/leetcode/66-Plus-one.py
+++ Test tasks (unsorted)/Solutions/leetcode/66-Plus-one.py
@@ -11,6 +11,15 @@ integer does not contain any leading 0's.
 Increment the large integer by one and return the resulting array of digits.
 """
 
+# solution with divmod
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        for i in range(len(digits)-1, -1, -1):
+            carry, digits[i] = divmod(digits[i] + carry, 10)
+        return digits if not carry else [carry] + digits
+    
+# solution with convert to str|int
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         return list(map(int, str(int(''.join(map(str, digits))) + 1)))
