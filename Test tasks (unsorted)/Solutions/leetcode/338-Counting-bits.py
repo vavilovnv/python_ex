8d1499d709b18835eba15fda5b9ccc777297d056
Николай <53932721+vavilovnv@users.noreply.github.com>
2024-03-12 11:27:25 +0300

    Add the new approach

--- Test tasks (unsorted)/Solutions/leetcode/338-Counting-bits.py
+++ Test tasks (unsorted)/Solutions/leetcode/338-Counting-bits.py
@@ -7,6 +7,7 @@ Given an integer n, return an array ans of length n + 1 such that for each i
 (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 """
 
+# approach 1
 class Solution:
     def countBits(self, n: int) -> List[int]:
         res = [0]
@@ -16,4 +17,12 @@ class Solution:
                 count += i & 1
                 i >>= 1
             res.append(count)
+        return res
+
+# approach 2
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0] * (n + 1)
+        for i in range(1, n+1):
+            res[i] = res[i >> 1] + i % 2
         return res
