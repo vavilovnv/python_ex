7e2aa4aee91b164ad8f79b1234a8c078f856005f
Николай <53932721+vavilovnv@users.noreply.github.com>
2024-03-31 09:23:06 +0300

    New approach added

--- Test tasks (unsorted)/Solutions/leetcode/53-Maximum-subarray.py
+++ Test tasks (unsorted)/Solutions/leetcode/53-Maximum-subarray.py
@@ -7,17 +7,22 @@ Given an integer array nums, find the subarray with the largest sum, and
 return its sum.
 """
 
+# TC: O(n)
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        res, s = nums[0], 0
+        res, curr = nums[0], 0
         for num in nums:
-            if s < 0:
-                s = 0
-            s += num
-            res = max(res, s)
+            if curr < 0:
+                curr = 0
+            curr += num
+            res = max(res, curr)
         return res
 
-"""
-hint
-if s < 0 - it's bad sequence. take the max from all s =)
-"""
+# approach 2
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        cur_sum, max_sum = nums[0], nums[0]
+        for i in range(1, len(nums)):
+            cur_sum = max(cur_sum + nums[i],  nums[i])
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
