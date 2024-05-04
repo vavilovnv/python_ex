fd54456caf90debbe5691391ea7418a18e1af490
Николай <53932721+vavilovnv@users.noreply.github.com>
2024-05-04 17:51:34 +0300

    New approach added

--- Test tasks (unsorted)/Solutions/leetcode/2441-Largest-positive-integer-that-exists-with-its-negative.py
+++ Test tasks (unsorted)/Solutions/leetcode/2441-Largest-positive-integer-that-exists-with-its-negative.py
@@ -9,10 +9,19 @@ positive integer k such that -k also exists in the array.
 Return the positive integer k. If there is no such integer, return -1.
 """
 
+# approach 1
 class Solution:
     def findMaxK(self, nums: List[int]) -> int:
         res, st = -1, set(nums)
-        for num in nums :
-            if -num in st and num in st :
-                res = max(res, num, -num)
+        for num in nums:
+            if num > 0 and -num in st:
+                res = max(res, num)
         return res
+
+
+# approach 2
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        neg = {num for num in nums if num < 0}
+        res = [num for num in nums if num > 0 and -num in neg]
+        return max(res) if res else -1
