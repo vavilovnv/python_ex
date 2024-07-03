c103442eae36b61863a8a4ddbd383d97acc70fb6
Николай <53932721+vavilovnv@users.noreply.github.com>
2024-07-03 12:12:03 +0300

    Approach changed

--- Test tasks (unsorted)/Solutions/leetcode/350-Intersection-of-two-arrays-II.py
+++ Test tasks (unsorted)/Solutions/leetcode/350-Intersection-of-two-arrays-II.py
@@ -10,23 +10,16 @@ in both arrays and you may return the result in any order.
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res = []
+        res, i, j = [], 0, 0
         nums1.sort()
-        for num in nums2:
-            if not nums1:
-                break
-            left, right = 0, len(nums1) - 1
-            while left <= right:
-                mid = left + (right - left) // 2
-                if nums1[mid] == num:
-                    res.append(nums1.pop(mid))
-                    break 
-                if nums1[mid] < num:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+        nums2.sort()
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums2[j] < nums1[i]:
+                j += 1
+            else:
+                res.append(nums1[i])
+                i += 1
+                j += 1
         return res
-
-"""
-use binary search
-"""
